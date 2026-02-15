37290d07aaba8bf1e5d4cc5e063969ebd1fabeac
Samuel Whitehall <samuelwhitehall@gmail.com>
2026-02-14 23:59:07 -0500

    added avg over a number of trials defined my the user

--- testing-setup/six6.py
+++ testing-setup/six6.py
@@ -1,12 +1,11 @@
 import random
 import time
-limit = 10000000
 
-i = 0
+number_of_trials = 1000000
+running_total = 0
 
 number = 1
-goal_times_in_a_row = 7
-times_in_a_row = 0
+goal_times_in_a_row = 2
 
 def roll(number, times_in_a_row):
     if random.randint(1,6) == number:
@@ -18,10 +17,25 @@ def roll(number, times_in_a_row):
         
     
 
+def find_once(goal_times_in_a_row, number):
+    i = 0
+    times_in_a_row = 0
+    while times_in_a_row < goal_times_in_a_row:
+        
+        times_in_a_row = roll(number, times_in_a_row)
 
-while i < limit and times_in_a_row < goal_times_in_a_row:
-    times_in_a_row = roll(number, times_in_a_row)
+        i += 1
 
-    i += 1
+    return i
 
-print(f"it took {i} rolls to get {goal_times_in_a_row} {number}'s in a row")
+
+# x = find_once(i, times_in_a_row, goal_times_in_a_row, number)
+# print(f"it took {x} rolls to get {goal_times_in_a_row} {number}'s in a row")
+
+for trial in range(number_of_trials):
+
+    trial_result = find_once(goal_times_in_a_row, number)
+
+    running_total += trial_result
+ 
+print("final average: " + str(running_total/number_of_trials))
